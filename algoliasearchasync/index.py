40e5d6a058c26e9649218b09b2b4248db709e9ee
Léo Ercolanelli <leo.ercolanelli@gmail.com>
2017-05-16 11:01:34 +0200

    implement browse_from, browse_all and delete_by_query

--- algoliasearchasync/index.py
+++ algoliasearchasync/index.py
@@ -30,7 +30,44 @@ INDEX_ASYNC_METHODS = [
     'search_synonyms',
     'set_settings',
     'update_user_key',
+    'browse_from',
 ]
+
+class AsyncIndexIterator:
+    def __init__(self, index, params=None):
+        if params is None:
+            params = {}
+
+        self.index = index
+        self.params = params
+        self.cursor = None
+
+    @asyncio.coroutine
+    def __aiter__(self):
+        yield from self._load_next_page()
+        return self
+
+    @asyncio.coroutine
+    def __anext__(self):
+        return self._next()
+
+    @asyncio.coroutine
+    def _next(self):
+        while True:
+            if self.pos < len(self.answer['hits']):
+                self.pos += 1
+                return self.answer['hits'][self.pos - 1]
+            elif self.cursor:
+                yield from self._load_next_page()
+                continue
+            else:
+                raise StopAsyncIteration
+
+    @asyncio.coroutine
+    def _load_next_page(self):
+        self.answer = yield from self.index.browse_from_async(self.params, self.cursor)
+        self.pos = 0
+        self.cursor = self.answer.get('cursor', None)
 
 
 class IndexAsync:
@@ -51,3 +88,30 @@ class IndexAsync:
             if res['status'] == 'published':
                 return res
             yield from asyncio.sleep(time_before_retry / 1000)
+
+    @asyncio.coroutine
+    def delete_by_query_async(self, query, params=None):
+        if params is None:
+            params = {}
+
+        params['query'] = query
+        params['hitsPerPage'] = 1000
+        params['attributesToRetrieve'] = ['objectID']
+        params['attributesToSnippet'] = []
+        params['attributesToHighlight'] = []
+        params['distinct'] = []
+
+        ids = []
+        iterator = yield from AsyncIndexIterator(self, params).__aiter__()
+
+        try:
+            while True:
+                next = yield from iterator.__anext__()
+                ids.append(next['objectID'])
+        except StopAsyncIteration:
+            pass
+
+        return (yield from self.delete_objects_async(ids))
+
+    def browse_all_async(self, params=None):
+        return AsyncIndexIterator(self, params=params)
